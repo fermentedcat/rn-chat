c69eaeccd99910bc4004b70b2d7535aa86107c10
Maja Thunberg <majaneko@gmail.com>
2021-11-17 16:56:25 +0100

    Edit styling

--- app/components/CustomButton.js
+++ app/components/CustomButton.js
@@ -1,37 +1,38 @@
 import React from 'react'
 import { Pressable, Text, View, StyleSheet, Platform } from 'react-native'
 
-import getColorBrightness from '../utils/getColorBrightness'
+import getColorBrightness from '../utils/colors/getColorBrightness'
+import darkenHex from '../utils/colors/darkenHex'
 import colors from '../styles/colors'
 import theme from '../styles/theme'
 
-function CustomButton({ title, handlePress, bgColor, rippleColor }) {
+function CustomButton({ title, onPress, bgColor }) {
   const isLightBg = getColorBrightness(bgColor) > 200
   const textColor = isLightBg ? colors.black : colors.white
 
   return (
-    <View style={styles(bgColor, rippleColor, textColor).buttonWrapper}>
+    <View style={styles(bgColor, textColor).buttonWrapper}>
       <Pressable
-        onPress={handlePress}
+        onPress={onPress}
         style={({pressed}) => [{
-          backgroundColor: pressed && Platform.OS === 'ios' ? rippleColor : bgColor
+          backgroundColor: pressed && Platform.OS === 'ios' ? darkenHex(bgColor, 5) : bgColor
         }, styles().button]}
-        android_ripple={{ color: rippleColor }}
+        android_ripple={{ color: darkenHex(bgColor, 15) }}
       >
-        <Text style={styles(bgColor, rippleColor, textColor).buttonText}>{title}</Text>
+        <Text style={styles(bgColor, textColor).buttonText}>{title}</Text>
       </Pressable>
     </View>
   )
 }
 
-const styles = (bgColor, rippleColor, textColor) => StyleSheet.create({
+const styles = (bgColor, textColor) => StyleSheet.create({
   buttonWrapper: {
     marginBottom: 10,
     overflow: 'hidden',
     height: 65,
     borderRadius: 65 / 2,
     justifyContent: 'center',
-    backgroundColor: bgColor
+    backgroundColor: colors[bgColor]
   },
   button: {
     flex: 1,
