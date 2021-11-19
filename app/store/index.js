452d869b199c8e56f4f4ec6eaeb687a42a6660cb
Maja Thunberg <majaneko@gmail.com>
2021-11-19 17:11:29 +0100

    Remove redux serializableCheck

--- app/store/index.js
+++ app/store/index.js
@@ -4,5 +4,9 @@ import authSlice from './auth-slice'
 export const store = configureStore({
   reducer: {
     auth: authSlice,
-  }
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 })
